b4cc4bf0aa114fe63c32f1867fe6b07daaec0fd0
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-05-06 13:48:46 +0530

    practice

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -1,57 +1,51 @@
 resource "aws_key_pair" "eks" {
   key_name   = "expense-eks"
-  public_key = file("/Users/lokeshpeddireddy999/.ssh/eks.pub")
+  public_key = file("~/.ssh/eks.pub") # Using home dir shortcut for portability
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = local.name
-  cluster_version = "1.31" # later we upgrade 1.32
-  create_node_security_group = false
-  create_cluster_security_group = false
-  cluster_security_group_id = local.eks_control_plane_sg_id
-  node_security_group_id = local.eks_node_sg_id
+  cluster_name                    = local.name
+  cluster_version                 = "1.27"
+  create_node_security_group      = false
+  create_cluster_security_group   = false
+  cluster_security_group_id       = local.eks_control_plane_sg_id
+  node_security_group_id          = local.eks_node_sg_id
 
-  #bootstrap_self_managed_addons = false
   cluster_addons = {
     coredns                = {}
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
-    metrics-server = {}
+    metrics-server         = {}
   }
 
-  # Optional
   cluster_endpoint_public_access = false
-
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
   vpc_id                   = local.vpc_id
   subnet_ids               = local.private_subnet_ids
   control_plane_subnet_ids = local.private_subnet_ids
 
-  # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
   }
 
   eks_managed_node_groups = {
     blue = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      #ami_type       = "AL2_x86_64"
       instance_types = ["m5.xlarge"]
-      key_name = aws_key_pair.eks.key_name
+      key_name       = aws_key_pair.eks.key_name
 
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
+      min_size       = 2
+      max_size       = 10
+      desired_size   = 2
+
       iam_role_additional_policies = {
-        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        AmazonEFSCSIDriverPolicy = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-        AmazonEKSLoadBalancingPolicy = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+        AmazonEBSCSIDriverPolicy      = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+        AmazonEFSCSIDriverPolicy      = "arn:aws:iam::aws:policy/AmazonEFSCSIDriverPolicy"
+        AmazonEKSLoadBalancingPolicy  = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
       }
     }
   }
@@ -59,7 +53,7 @@ module "eks" {
   tags = merge(
     var.common_tags,
     {
-        Name = local.name
+      Name = local.name
     }
   )
 }
