53b7d688546b7411d5ff784131980603bd4e1df2
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-05-06 11:02:23 +0530

    prcatice

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -2,13 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.84.0"
+      version = ">= 5.95.0"
+
     }
   }
 
   backend "s3" {
     bucket = "82s-tf-remotestate-development"
-    key    = "expense-dev-eks-bastion" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+    key    = "expense-dev-eks-eks" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "82s-tf-remotestate-development"
   }
