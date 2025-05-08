80ca92d291f5bef0547303db492db71e222772eb
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-05-08 17:09:49 +0530

    practice

---
+++ destroy-all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE_DIR="terraform-aws-eks"
+
+# Loop through all subdirectories
+for dir in "$BASE_DIR"/*; do
+  if [ -d "$dir" ] && ls "$dir"/*.tf > /dev/null 2>&1; then
+    echo "Destroying Terraform resources in $dir..."
+    cd "$dir"
+    
+    terraform init -input=false
+    terraform destroy -auto-approve
+
+    cd - > /dev/null
+  fi
+done
