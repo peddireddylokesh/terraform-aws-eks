daf96a9f9de1e6dffe830d15e941471e81a34d2c
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-05-08 16:37:56 +0530

    practice

--- 60-ingress-alb/paramters.tf
+++ 60-ingress-alb/paramters.tf
@@ -2,4 +2,5 @@ resource "aws_ssm_parameter" "ingress_alb_certificate_arn" {
   name  = "/${var.project_name}/${var.environment}/ingress_alb_certificate_arn"
   type  = "String"
   value = aws_lb_listener.https.arn
+  overwrite = true
 }
